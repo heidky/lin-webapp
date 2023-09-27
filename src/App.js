35f50048f5858836ddcad4e04447a90f7e132a86
Heid Ky <heid.ky8@gmail.com>
2023-09-27 13:24:32 +0200

    Created base app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="min-h-screen flex flex-col items-stretch justify-stretch bg-gray-900">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-white text-4xl">Hello world!</h1>
       </header>
+
     </div>
   );
 }
